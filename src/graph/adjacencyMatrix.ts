b905a4b12ddb0c21a3ee8be47af16dab30cf2f85
chris-PC\chris <cwilson8706@gmail.com>
2020-01-20 19:20:08 -0500

    switch to ts (rough)

--- src/graph/adjacencyMatrix.ts
+++ src/graph/adjacencyMatrix.ts
@@ -7,9 +7,9 @@ interface Edge {
 }
 
 interface Graph {
-  graph: Array<string>;
-  vertices: Record<string, Node>;
-  verticesList: Record<string, Node>;
+  graph: Edge[][];
+  vertices: Map<string, Node>;
+  verticesList: Map<string, Node>;
   insert: (u: Node, v: Node, w: number) => void;
   remove: (u: Node) => Node;
   getEdge: (u: Node, v: Node) => Edge;
@@ -19,3 +19,44 @@ interface Graph {
   addVertex: (v: Node) => void;
   addEdge: (u: Node, v: Node, w: number) => void;
 }
+
+class AdjacencyMatrix implements Graph {
+  graph: Edge[][] = [];
+  vertices: Map<string, Node> = new Map<string, Node>();
+  verticesList: Map<string, Node> = new Map<string, Node>();
+  insert(u: Node, v: Node, w: number) {
+
+  }
+
+  remove(u: Node): Node {
+
+  }
+
+  getEdge(u: Node, v: Node): Edge {
+
+  }
+
+  getEdges(u: Node): Edge[] {
+
+  }
+
+  getVertices(): Node[] {
+
+  }
+
+  getVertex(value: number): Node {
+
+  }
+
+  addVertex(v: Node) {
+
+  }
+
+  addEdge(u: Node, v: Node, w: number) {
+
+  }
+
+  constructor(): AdjacencyMatrix {  };
+};
+
+export AdjacencyMatrix;
